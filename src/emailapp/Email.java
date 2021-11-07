bb979f8a316194d0ea465f3a67a4abd6fbc8819b
TatianaMatveeva <tanya-matveeva@bk.ru>
2021-11-07 22:11:44 +0100

    Update 1

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,10 +7,13 @@ public class Email {
 
     private String firstName;
     private String lastName;
-    private String password; //Generate random password
+    private String password;
     private String department;
-    private int mailboxCapacity; //Set the mailbox capacity
+    private String email;
+    private int defaultPasswordLenght = 10;
+    private int mailboxCapacity;
     private String alternateEmail;
+    private String companySuffix = "pnl.pl";
 
     // Constructor to receive the first name and last name.
     public Email(String firstName, String lastName) {
@@ -19,9 +22,16 @@ public class Email {
         System.out.println("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
 
     //Call a method asking for the department - return the department
-
     this.department = setDepartment();
     System.out.println("Department; " + this.department);
+
+    //Call a method that returns a random password
+     this.password = randomPassword(defaultPasswordLenght);
+        System.out.println("Your password is:" + this.password);
+
+    //Combine elements to generate email
+     email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department+ "." + companySuffix;
+        System.out.println("Your email is: " + email);
 }
     // Ask for the department
     private String setDepartment(){
@@ -34,10 +44,26 @@ public class Email {
         else {return"";}
 
     }
-
-
-
-
-
+    //Generate random password
+    public String randomPassword(int lenght) {
+        String passwordSet ="ABCDEFGHIKLMNOQRSTUWXYZ1234567890!@#$%";
+        char[] password = new char[lenght];
+        for (int i=0; i<lenght; i++){
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+    //Set the mail capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
+    //Set the alternate mail
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
     //Change the password
+    public void changePassword(String  password) {
+        this.password = password;
+    }
 }
